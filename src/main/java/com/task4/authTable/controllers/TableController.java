6b729405cd3019f2e2b53d5c265edc0baebfa83b
Makkksx <m.vologdin@mail.ru>
2021-09-17 18:31:20 +0300

    fix block function

--- src/main/java/com/task4/authTable/controllers/TableController.java
+++ src/main/java/com/task4/authTable/controllers/TableController.java
@@ -4,17 +4,14 @@ import com.task4.authTable.models.Status;
 import com.task4.authTable.models.User;
 import com.task4.authTable.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Controller
 public class TableController {
@@ -23,26 +20,16 @@ public class TableController {
 
     @GetMapping("/table")
        public String tableMain(Model model) {
-        model.addAttribute("users", userRepository);
+        model.addAttribute("users", userRepository.findAll());
         model.addAttribute("title", "Table here");
         return "table";
     }
-    @GetMapping("/table/block/{ids}")
-    public String block(@PathVariable("ids") String ids, Model model) {
+    @GetMapping("/table/setBlock/{status}/{ids}")
+    public String setBlock(@PathVariable("status") String status, @PathVariable("ids") String ids, Model model) {
         List<Long> list = Arrays.stream(ids.split(",")).map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
         Iterable<User> allById = userRepository.findAllById(list);
         for (User el:allById){
-            el.setStatus(Status.BANNED);
-        }
-        userRepository.saveAll(allById);
-        return "redirect:/table";
-    }
-    @GetMapping("/table/unblock/{ids}")
-    public String unblock(@PathVariable("ids") String ids, Model model) {
-        List<Long> list = Arrays.stream(ids.split(",")).map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
-        Iterable<User> allById = userRepository.findAllById(list);
-        for (User el:allById){
-            el.setStatus(Status.ACTIVE);
+            el.setStatus(Status.valueOf(status));
         }
         userRepository.saveAll(allById);
         return "redirect:/table";
