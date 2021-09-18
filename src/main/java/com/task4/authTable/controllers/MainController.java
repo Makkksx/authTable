a7284a072cb58df3ce392e6a0bd0a4adde07cb84
Makkksx <m.vologdin@mail.ru>
2021-09-18 10:24:49 +0300

    fix logout (no csrf)

--- src/main/java/com/task4/authTable/controllers/MainController.java
+++ src/main/java/com/task4/authTable/controllers/MainController.java
@@ -1,15 +1,11 @@
 package com.task4.authTable.controllers;
 
-import com.task4.authTable.models.User;
 import com.task4.authTable.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Collections;
 
 
 @Controller
